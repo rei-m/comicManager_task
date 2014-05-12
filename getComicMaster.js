046fcbe592fc2d190e08a783d90a3a70d7ae715f
rei-m <star-oceans@abelia.ocn.ne.jp>
2014-05-12 17:34:52 +0900

    add ComicMaster AND refactor getComicMaster

--- getComicMaster.js
+++ getComicMaster.js
@@ -1,15 +1,37 @@
-// モジュール読み込み
-var db = require("mod/cmnDb"),
-    bl = require("mod/getComicMasterBL"),
-    logger = require('mod/logger');
+/**************************************************************************************************
+ * モジュール名　: 漫画マスタ情報取得API
+ * 概要　　　　　: 漫画マスタ情報を取得するAPIコントローラ
+ *************************************************************************************************/
 
+// モジュール読み込み
+module.paths.push('./my_modules/node_modules');
+var Q = require('q'),
+		db = require('cmn/cmnDb'),
+		cmnFunc = require('cmn/cmnFunc'),
+		getComiclist = require('getComicMaster/getComicList'),
+		getComicListFromSales = require('getComicMaster/getComicListFromSales'),
+		saveComicList = require('getComicMaster/saveComicList'),
+		CONST = require('getComicMaster/const');
+
+// 処理開始
 (function(){
 
-  // 処理開始ログ
-
-  // コミック情報を取得してDBに保存する
-  bl.getAndSaveComicList(db);
-
-  // 処理終了ログ
+	Q.when(CONST.FUNC_NAME)
+		// 初期処理
+		.then(cmnFunc.initProc)
+		// スクレイピングでコミック情報を取得
+		.then(getComiclist)
+		// 新刊情報からコミック情報を取得
+		.then(function(in_comicList){
+			return getComicListFromSales(in_comicList, db);
+		})
+		.fail(cmnFunc.errorProc)
+		// 取得したコミック情報をDBに登録
+		.then(function(in_comicList){
+			return saveComicList(in_comicList, db);
+		})
+		.fail(cmnFunc.errorProc)
+		// 終了処理
+		.done(cmnFunc.endProc);
 
 })();
--- getComicSalesDate.js
+++ getComicSalesDate.js
@@ -4,11 +4,6 @@ var db = require("mod/cmnDb"),
 
 (function(){
 
-  // 処理開始ログ
-
-  // コミック情報を取得してDBに保存する
   bl.getAndSaveComicList(db);
-
-  // 処理終了ログ
 
 })();
